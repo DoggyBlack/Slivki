3da391712123bd74f9875eaeaa12b2a0e3e085c3
Sobaka <andriy.vakulyuk1998@gmail.com>
2017-03-31 20:13:25 +0300

    Created version for laptops

--- project/build/ajax.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-  url: "ajax.html",
-  cache: false,
-  type: "GET",
-  dataType: "html",
-  success: function(html){
-    $(".information").append(html);
-  }
-});
--- project/build/main.js
+++ project/build/main.js
@@ -1,4 +1,24 @@
-var right = function(){
-  $('.header__nav').show();
-  $('.menu').hide();
-}
+$(document).ready(function(){
+  $("#content").hide();
+    $("#menu").click(function(){
+      if ($("#content").is(":hidden")) {
+
+        $("#content").show("slow");
+
+      } else {
+
+        $("#content").hide("slow");
+
+                     }
+    return false;
+  });
+});
+$(document).ready(function(){
+  $("#calculation-content").hide();
+    $("#calculation").click(function () {
+        $("#calculation-content").show("slow");
+    });
+    $("#close").click(function(){
+        $("#calculation-content").hide("slow");
+  });
+});
